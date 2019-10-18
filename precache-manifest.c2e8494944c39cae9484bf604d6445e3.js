3cafcc7035d5c5f4809635225bfcf792afa41180
Renzo Herrera <renzo.herrera@softtek.com>
2019-10-18 11:58:33 -0300

    Updates

---
+++ precache-manifest.c2e8494944c39cae9484bf604d6445e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef18e276e84029d33aaa042cfc33087",
+    "url": "/reactpage/index.html"
+  },
+  {
+    "revision": "bbeeecff8532299695c2",
+    "url": "/reactpage/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e4bcbc030615aacb51c7",
+    "url": "/reactpage/static/js/2.c9218511.chunk.js"
+  },
+  {
+    "revision": "bbeeecff8532299695c2",
+    "url": "/reactpage/static/js/main.8497a92b.chunk.js"
+  },
+  {
+    "revision": "236f7fb7d45883c0ec8f",
+    "url": "/reactpage/static/js/runtime-main.74365665.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactpage/static/media/logo.25bf045c.svg"
+  }
+]);
